7af593db88f6ef10dcd1d670d7b66983637fc8df
Roman Kostetskyi <roman.kostetskyi@eleks.com>
2020-05-14 14:22:49 +0300

    add fontawesome; add todolist component; add header component; add login component; markup;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-module.routing';
+import { TodolistComponent } from './todolist/todolist.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    TodolistComponent,
+    HeaderComponent,
+    LoginComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
