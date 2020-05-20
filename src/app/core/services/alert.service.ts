8e13bc8e8d8f5e2810ca7dcdb5dd1e614ea0fe95
Roman Kostetskyi <roman.kostetskyi@eleks.com>
2020-05-20 15:22:46 +0300

    fix alert

--- src/app/core/services/alert.service.ts
+++ src/app/core/services/alert.service.ts
@@ -2,37 +2,47 @@ import { Injectable } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
 import { Observable, Subject } from 'rxjs';
 
+export interface Config {
+  keepAfterNavigationChange: boolean;
+  duration: number;
+}
 @Injectable()
 export class AlertService {
   private subject = new Subject<any>();
-  private keepAfterNavigationChange = false;
+  private keepAfterNavigationChange = true;
+  private duration = 3000;
 
   constructor(private router: Router) {
-    // clear alert message on route change
-    router.events.subscribe(event => {
-      if (event instanceof NavigationStart) {
-        if (this.keepAfterNavigationChange) {
-          // only keep for a single location change
-          this.keepAfterNavigationChange = false;
-        } else {
-          // clear alert
-          this.subject.next();
-        }
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationStart && this.keepAfterNavigationChange) {
+        this.subject.next();
       }
     });
   }
 
-  success(message: string, keepAfterNavigationChange = false) {
-    this.keepAfterNavigationChange = keepAfterNavigationChange;
-    this.subject.next({ type: 'success', text: message });
+  success(message: string, config?: Config) {
+    this.show('success', message, config);
   }
 
-  error(message: string, keepAfterNavigationChange = false) {
-    this.keepAfterNavigationChange = keepAfterNavigationChange;
-    this.subject.next({ type: 'error', text: message });
+  error(message: string, config?: Config) {
+    this.show('error', message, config);
+  }
+
+  remove() {
+    setTimeout(() => {
+      this.subject.next();
+    }, this.duration);
   }
 
   getMessage(): Observable<any> {
     return this.subject.asObservable();
+  }
+
+  private show(type: string, text: string, config?: Config) {
+    this.keepAfterNavigationChange =
+      config?.keepAfterNavigationChange || this.keepAfterNavigationChange;
+    this.duration = config?.duration || this.duration;
+    this.subject.next({ type, text });
+    this.remove();
   }
 }
